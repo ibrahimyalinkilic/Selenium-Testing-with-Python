750609e6a8a8852a86132ffa6ca73b6941ba7c65
ibrahimyalinkilic <53036936+ibrahimyalinkilic@users.noreply.github.com>
2019-07-29 14:34:57 +0300

    Add files via upload

--- SignIn.py
+++ SignIn.py
@@ -1,8 +1,7 @@
 import unittest
-import time
 from selenium import webdriver
+import time
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 
 
 class SignIn(unittest.TestCase):
@@ -13,18 +12,7 @@ class SignIn(unittest.TestCase):
         self.driver.maximize_window()
         self.driver.get("https://dev.bitexen.com/")
 
-    def test_succesful_login(self):
-
-        self.driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[1]/div/div/div[2]/a[1]").click()
-        self.driver.find_element_by_id("userName").send_keys("berkyalinkilic@gmail.com")
-        self.driver.find_element_by_id("password").send_keys("asparas1")
-        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
-        time.sleep(2)
-        x = self.driver.find_element(By.XPATH, "//*[@id='root']/div/div/div[1]/div/div[2]/div/div/div[2]").text
-        print(x)
-        self.assertEqual(x, "Hoş geldin,")
-
-    def test_wrong_password_login(self):
+    def test_login(self):
 
         self.driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[1]/div/div/div[2]/a[1]").click()
         self.driver.find_element_by_id("userName").send_keys("berkyalinkilic@gmail.com")
@@ -35,7 +23,9 @@ class SignIn(unittest.TestCase):
         print(x)
         self.assertEqual(x, "Kullanıcı adı ya da şifre hatalı.")
 
-    def test_wrong_mail_login(self):
+        self.driver.execute_script("window.history.go(-1)")
+        self.driver.refresh()
+
         self.driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[1]/div/div/div[2]/a[1]").click()
         self.driver.find_element_by_id("userName").send_keys("berkyalinkilicc@gmail.com")
         self.driver.find_element_by_id("password").send_keys("asparas1")
@@ -45,27 +35,26 @@ class SignIn(unittest.TestCase):
         print(x)
         self.assertEqual(x, "Customer not found.")
 
-    def test_invalid_login(self):
+        self.driver.execute_script("window.history.go(-1)")
+        self.driver.refresh()
+
         self.driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[1]/div/div/div[2]/a[1]").click()
         self.driver.find_element_by_id("userName").send_keys("abc")
         self.driver.find_element_by_id("password").send_keys("abc")
         self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
         time.sleep(1)
-        x = self.driver.find_element(By.ID,"userName-helper-text").text
+        x = self.driver.find_element(By.ID, "userName-helper-text").text
         print(x)
         self.assertEqual(x, "Geçersiz e-posta adresi")
 
-    def is_element_present(self,how,what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException:
-            return False
-        return True
+        self.driver.execute_script("window.history.go(-1)")
+        self.driver.refresh()
 
-    def tearDown(self):
-
-        self.driver.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[1]/div/div/div[2]/a[1]").click()
+        self.driver.find_element_by_id("userName").send_keys("berkyalinkilic@gmail.com")
+        self.driver.find_element_by_id("password").send_keys("asparas1")
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
+        time.sleep(2)
+        x = self.driver.find_element(By.XPATH, "//*[@id='root']/div/div/div[1]/div/div[2]/div/div/div[2]").text
+        print(x)
+        self.assertEqual(x, "Hoş geldin,")
--- SignUp.py
+++ SignUp.py
@@ -1,41 +1,87 @@
 from selenium import webdriver
-
-class SignUp:
-
-    def __init__(self,userName,name,surName,password,referrerCode):
-        self.userName = userName
-        self.name = name
-        self.surName = surName
-        self.password = password
-        self.referrerCode = referrerCode
-
-    def signup(self):
-
-        driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[1]/div/div/div[2]/a[2]/span[1]").click()
-        driver.find_element_by_id("userName").send_keys(self.userName)
-        driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
-        driver.find_element_by_id("name").send_keys(self.name)
-        driver.find_element_by_id("surname").send_keys(self.surName)
-        driver.find_element_by_id("password").send_keys(self.password)
-        driver.find_element_by_id("password2").send_keys(self.password)
-        driver.find_element_by_id("referrerCode").send_keys(self.referrerCode)
-        driver.find_element_by_xpath("//*[@id='root']/div/div[3]/div[8]/span/span[1]/input").click()
-        driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
-
-    def check(self):
-        xpath = "//*[@id='root']/div/div[3]/div[9]/div"
-        if driver.find_element_by_xpath(xpath) == True:
-            print(self.userName + "Adlı bir kullanıcı zaten var.")
-
-        elif driver.find_element_by_xpath(xpath) == False:
-            print("Kayıt Başarılı.")
+import unittest
+from selenium.webdriver.common.by import By
+import time
 
 
-driver = webdriver.Chrome()
-url = "https://dev.bitexen.com/"
-driver.get(url)
-driver.maximize_window()
+class SignUp(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("https://dev.bitexen.com/")
+
+    def test_signup(self):
+
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[1]/div/div/div[2]/a[2]/span[1]").click()
+        self.driver.find_element_by_id("userName").send_keys("berkyalinkilic@gmail.com")
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
+        self.driver.find_element_by_id("name").send_keys("İbrahim")
+        self.driver.find_element_by_id("surname").send_keys("Yalınkılıç")
+        self.driver.find_element_by_id("password").send_keys("asparas1")
+        self.driver.find_element_by_id("password2").send_keys("asparas1")
+        self.driver.find_element_by_id("referrerCode").send_keys("")
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/div[8]/span/span[1]/input").click()
+        self. driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
+        time.sleep(2)
+        x = self.driver.find_element(By.XPATH,"//*[@id='root']/div/div[3]/div[9]/div").text
+        print(x)
+        self.assertEqual(x, 'Kullanıcı zaten mevcut.')
+
+        self.driver.execute_script("window.history.go(-1)")
+        self.driver.refresh()
+
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[1]/div/div/div[2]/a[2]/span[1]").click()
+        self.driver.find_element_by_id("userName").send_keys("berkyalinkilic")
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
+        self.driver.find_element_by_id("name").send_keys("İbrahim")
+        self.driver.find_element_by_id("surname").send_keys("Yalınkılıç")
+        self.driver.find_element_by_id("password").send_keys("asparas1")
+        self.driver.find_element_by_id("password2").send_keys("asparas1")
+        self.driver.find_element_by_id("referrerCode").send_keys("")
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/div[8]/span/span[1]/input").click()
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
+        time.sleep(2)
+        x = self.driver.find_element(By.ID,"userName-helper-text").text
+        print(x)
+        self.assertEqual(x, 'Geçersiz e-posta adresi')
+
+        self.driver.execute_script("window.history.go(-1)")
+        self.driver.refresh()
+
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[1]/div/div/div[2]/a[2]/span[1]").click()
+        self.driver.find_element_by_id("userName").send_keys("berkyalinkilic@gmail.com")
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
+        self.driver.find_element_by_id("name").send_keys("İbrahim")
+        self.driver.find_element_by_id("surname").send_keys("Yalınkılıç")
+        self.driver.find_element_by_id("password").send_keys("")
+        self.driver.find_element_by_id("password2").send_keys("")
+        self.driver.find_element_by_id("referrerCode").send_keys("")
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/div[8]/span/span[1]/input").click()
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
+        time.sleep(2)
+        x = self.driver.find_element(By.ID, "password-helper-text").text
+        print(x)
+        self.assertEqual(x, 'Boş bırakılamaz')
+
+        self.driver.execute_script("window.history.go(-1)")
+        self.driver.refresh()
 
 
-user1 = SignUp("something", "ibrahim", "yalinkilic", "asdsdaf23", " ")
-user1.signup()
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[1]/div/div/div[2]/a[2]/span[1]").click()
+        self.driver.find_element_by_id("userName").send_keys("berkyalinkilic@gmail.com")
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
+        self.driver.find_element_by_id("name").send_keys("İbrahim")
+        self.driver.find_element_by_id("surname").send_keys("Yalınkılıç")
+        self.driver.find_element_by_id("password").send_keys("asparas1")
+        self.driver.find_element_by_id("password2").send_keys("asparas11")
+        self.driver.find_element_by_id("referrerCode").send_keys("")
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/div[8]/span/span[1]/input").click()
+        self.driver.find_element_by_xpath("//*[@id='root']/div/div[3]/button/span[1]").click()
+        time.sleep(2)
+        x = self.driver.find_element(By.XPATH, "//*[@id='root']/div/div[3]/div[9]/div").text
+        print(x)
+        self.assertEqual(x, 'Girdiğiniz şifreler eşleşmiyor')
+
+
+
